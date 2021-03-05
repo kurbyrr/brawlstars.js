081bcd0f17e89524e64e3ce74fa95ff50557a6a7
Ralph Geha <ralph.geha@gmail.com>
2021-03-05 09:07:50 +0200

    Added baseURL as constructor option

--- src/Client.ts
+++ src/Client.ts
@@ -1,14 +1,15 @@
-import { Player, PlayerBattlelog, RankingsPlayer } from "./Player";
+import Cache, { Options } from "node-cache";
+import fetch from "node-fetch";
+import { stringify } from "querystring";
 import { Brawler } from "./Brawler";
 import { Club, ClubMember, RankingsClub } from "./Club";
-import Cache, { Options } from "node-cache";
-import fetch, { Response } from "node-fetch";
+import { Player, PlayerBattlelog, RankingsPlayer } from "./Player";
 import { cleanTag } from "./utils";
-import { stringify } from "querystring";
 
 export interface ClientOptions {
   cache: boolean;
   cacheOptions?: Options;
+  baseURL?: string;
 }
 
 export class APIError extends Error {
@@ -23,11 +24,12 @@ export class APIError extends Error {
 export class Client {
   public token: string;
   public cache?: Cache;
-  public baseURL: string = "https://api.brawlstars.com/v1";
+  public baseURL: string;
 
   public constructor(token: string, options: ClientOptions = { cache: true }) {
     this.token = token;
     this.cache = options.cache ? new Cache(options.cacheOptions) : undefined;
+    this.baseURL = options.baseURL ?? "https://api.brawlstars.com/v1";
   }
 
   /**
@@ -41,85 +43,128 @@ export class Client {
   private async _fetch<T>(path: string, query?: any): Promise<T> {
     const url = this.baseURL + path + (query ? "?" + stringify(query) : "");
     const exists = this.cache?.get<T>(url);
-    if(exists) return exists;
+    if (exists) return exists;
 
     const response = await fetch(url, {
       headers: {
-        "Authorization": this.authorization,
+        Authorization: this.authorization,
         "User-Agent": "BrawlStars.js https://github.com/pollen5/brawlstars.js",
-        "Accept": "application/json"
-      }
+        Accept: "application/json",
+      },
     });
 
-    if(!response.ok) throw new APIError(response.statusText, response.status);
+    if (!response.ok) throw new APIError(response.statusText, response.status);
 
     const data = await response.json();
     const cache = response.headers.get("cache-control");
-    const ttl = cache && cache.startsWith("max-age=") ? parseInt(cache.slice(8)) : 0;
+    const ttl =
+      cache && cache.startsWith("max-age=") ? parseInt(cache.slice(8)) : 0;
 
-    if(ttl) this.cache?.set(url, data, ttl);
+    if (ttl) this.cache?.set(url, data, ttl);
 
     return data;
   }
 
   public getPlayer(tag: string): Promise<Player> {
-    return this._fetch<Player>(`/players/%23${cleanTag(tag)}`)
-      .then((res) => {
-        // Thanks a lot supercell.
-        res.x3vs3Victories = (res as any)["3vs3Victories"];
-        return res;
-      });
+    return this._fetch<Player>(`/players/%23${cleanTag(tag)}`).then((res) => {
+      // Thanks a lot supercell.
+      res.x3vs3Victories = (res as any)["3vs3Victories"];
+      return res;
+    });
   }
 
   public getPlayerBattlelog(tag: string): Promise<PlayerBattlelog[]> {
-    return this._fetch<PlayerBattlelog[]>(`/players/%23${cleanTag(tag)}/battlelog`);
+    return this._fetch<PlayerBattlelog[]>(
+      `/players/%23${cleanTag(tag)}/battlelog`
+    );
   }
 
   public getClub(tag: string): Promise<Club> {
     return this._fetch<Club>(`/clubs/%23${cleanTag(tag)}`);
   }
 
-  public getPlayerRankings(country: string, { before, after, limit }: { before?: string, after?: string, limit?: number } = {}): Promise<RankingsPlayer[]> {
+  public getPlayerRankings(
+    country: string,
+    {
+      before,
+      after,
+      limit,
+    }: { before?: string; after?: string; limit?: number } = {}
+  ): Promise<RankingsPlayer[]> {
     const query: any = {};
-    if(before) query.before = before;
-    if(after) query.after = after;
-    if(limit) query.limit = limit;
+    if (before) query.before = before;
+    if (after) query.after = after;
+    if (limit) query.limit = limit;
 
     return this._fetch<RankingsPlayer[]>(`/rankings/${country}/players`, query);
   }
 
-  public getClubRankings(country: string, { before, after, limit }: { before?: string, after?: string, limit?: number } = {}): Promise<RankingsClub[]> {
+  public getClubRankings(
+    country: string,
+    {
+      before,
+      after,
+      limit,
+    }: { before?: string; after?: string; limit?: number } = {}
+  ): Promise<RankingsClub[]> {
     const query: any = {};
-    if(before) query.before = before;
-    if(after) query.after = after;
-    if(limit) query.limit = limit;
+    if (before) query.before = before;
+    if (after) query.after = after;
+    if (limit) query.limit = limit;
 
     return this._fetch<RankingsClub[]>(`/rankings/${country}/clubs`, query);
   }
 
-  public getBrawlerRankings(country: string, brawler: string, { before, after, limit }: { before?: string, after?: string, limit?: number } = {}): Promise<RankingsPlayer[]> {
+  public getBrawlerRankings(
+    country: string,
+    brawler: string,
+    {
+      before,
+      after,
+      limit,
+    }: { before?: string; after?: string; limit?: number } = {}
+  ): Promise<RankingsPlayer[]> {
     const query: any = {};
-    if(before) query.before = before;
-    if(after) query.after = after;
-    if(limit) query.limit = limit;
+    if (before) query.before = before;
+    if (after) query.after = after;
+    if (limit) query.limit = limit;
 
-    return this._fetch<RankingsPlayer[]>(`/rankings/${country}/brawlers/${brawler}`, query);
+    return this._fetch<RankingsPlayer[]>(
+      `/rankings/${country}/brawlers/${brawler}`,
+      query
+    );
   }
 
-  public getClubMembers(tag: string, { before, after, limit }: { before?: string, after?: string, limit?: number } = {}): Promise<ClubMember[]> {
+  public getClubMembers(
+    tag: string,
+    {
+      before,
+      after,
+      limit,
+    }: { before?: string; after?: string; limit?: number } = {}
+  ): Promise<ClubMember[]> {
     const query: any = {};
-    if(before) query.before = before;
-    if(after) query.after = after;
-    if(limit) query.limit = limit;
+    if (before) query.before = before;
+    if (after) query.after = after;
+    if (limit) query.limit = limit;
 
-    return this._fetch<ClubMember[]>(`/clubs/%23${cleanTag(tag)}/members`, query);
+    return this._fetch<ClubMember[]>(
+      `/clubs/%23${cleanTag(tag)}/members`,
+      query
+    );
   }
 
-  public getBrawlers({ before, after, limit }: { before?: string, after?: string, limit?: number } = {}): Promise<Brawler[]> {
+  public getBrawlers({
+    before,
+    after,
+    limit,
+  }: { before?: string; after?: string; limit?: number } = {}): Promise<
+    Brawler[]
+  > {
     const query: any = {};
-    if(before) query.before = before;
-    if(after) query.after = after;
-    if(limit) query.limit = limit;
+    if (before) query.before = before;
+    if (after) query.after = after;
+    if (limit) query.limit = limit;
 
     return this._fetch<Brawler[]>(`/brawlers`, query);
   }
